a8c281b9139a390b7f49af1e401514fb6634de64
PedroHIegler <pedroiegler1601@gmail.com>
2023-08-30 08:57:08 -0300

    feature: ia que reconhece rostos em webcam

---
+++ public/capture_faces_by_webcam.py
@@ -0,0 +1,28 @@
+import cv2
+
+haarRosto = cv2.CascadeClassifier("haarcascade/haarcascade_frontalface_default.xml")
+webcam = cv2.VideoCapture(0)  
+
+while True:
+    conectado, imagem = webcam.read()
+    imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    
+    rostosEncontrados = haarRosto.detectMultiScale(imagemCinza,
+                                                   scaleFactor=1.1,
+                                                   minNeighbors=3,
+                                                   minSize=(50,50),
+                                                   maxSize=(200,200)
+                                                   )
+    
+    for(x, y, largura, altura) in rostosEncontrados:
+        cv2.rectangle(imagem, (x,y), (x+largura,y+altura), (0,0,255), 4)
+    
+    cv2.waitKey(1)
+    cv2.imshow("webcam", imagem)
+    
+    if (cv2.waitKey(1) == ord('x')):
+        breakxx
+
+
+webcam.release()
+cv2.destroyAllWindows()
